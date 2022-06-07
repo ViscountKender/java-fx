f75901e8bb39f199498f4ce60e6240cea0690708
Viscount <siyanov.d@sar.garm.ru>
2022-06-07 23:28:49 +0400

    промежуточный коммит

--- network_chat/src/main/java/ru/gb/siyanov/network_chat/Controller.java
+++ network_chat/src/main/java/ru/gb/siyanov/network_chat/Controller.java
@@ -3,7 +3,7 @@ package ru.gb.siyanov.network_chat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
