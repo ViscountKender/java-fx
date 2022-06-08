d23b6231a1bbfd7d4b398a6dbc95483cf2e443dc
Viscount <siyanov.d@sar.garm.ru>
2022-06-09 01:03:17 +0400

    ready

--- network_chat/src/main/java/ru/gb/siyanov/network_chat/Controller.java
+++ network_chat/src/main/java/ru/gb/siyanov/network_chat/Controller.java
@@ -1,14 +1,26 @@
 package ru.gb.siyanov.network_chat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
-
+    private TextArea historyArea;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField userBoard;
+
+    public void clickCheckButton(ActionEvent actionEvent) {
+        final String userText = userBoard.getText();
+        if(userText.isBlank()){
+            return;
+        }
+        historyArea.appendText(userText + "\n");
+        userBoard.clear();
+        userBoard.requestFocus();
+
+
     }
 }
--- network_chat/src/main/java/ru/gb/siyanov/network_chat/NetworkChat.java
+++ network_chat/src/main/java/ru/gb/siyanov/network_chat/NetworkChat.java
@@ -11,7 +11,7 @@ public class NetworkChat extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NetworkChat.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 440);
         stage.setTitle("NetworkChat!");
         stage.setScene(scene);
         stage.show();
